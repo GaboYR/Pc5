7e4d811b441b4ad17f5d3059fbc7ac64d7b16602
GaboYR <gabriel.yarleque.r@uni.pe>
2024-11-29 13:16:51 -0500

    a

---
+++ script.sh
@@ -0,0 +1,10 @@
+# Move to the directory where the script is located
+cd WebXR-demo2
+# Create a virtual enviroment
+python3 -m venv env
+# Activate the virtual enviroment
+source env/bin/activate
+# Install the requirements
+pip install -r requirements.txt
+# Run the script
+python3 main.py
